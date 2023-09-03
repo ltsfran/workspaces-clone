9da36069b87a7d93e8c78b7fffe24a7b0d438df7
Luis Tupa <luis.tupa@neoauto.pe>
2023-09-03 13:37:04 -0500

    feat: add header component

---
+++ src/app/about/page.tsx
@@ -0,0 +1,16 @@
+import Header from '@/app/components/header'
+
+export default function Home () {
+  return (
+    <>
+      <Header />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>Explore the workspaces</h1>
+        <h1 className="font-bold">Explore the workspaces</h1>
+        <h1 className="font-sans">Explore the workspaces</h1>
+        <h1 className="font-switzer">Explore the workspaces</h1>
+        <h1 className="font-switzer font-bold">Explore the workspaces</h1>
+      </main>
+    </>
+  )
+}
